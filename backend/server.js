abda96585631e9076250edb7996cda351bf2a764
Isra Saleem <israsaleem86@gmail.com>
2026-08-18 18:23:14 +0500

    Prepare backend for production

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,16 @@ const contactRoutes = require("./contactRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "https://travel-website-theta-teal.vercel.app",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use("/api/packages", packageRoutes);
