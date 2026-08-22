83a58dee934ce7dbcd954cbbd612807547234225
Isra Saleem <israsaleem86@gmail.com>
2026-08-22 16:05:13 +0500

    Fix production API URL

--- app/admin/routes/create/page.tsx
+++ app/admin/routes/create/page.tsx
@@ -95,7 +95,7 @@ export default function CreateRoutePage() {
       // ----------------------------------------
 
       const response = await fetch(
-        "http://localhost:5000/api/packages",
+         `${process.env.NEXT_PUBLIC_API_URL}/api/packages`,
         {
           method: "POST",
 
